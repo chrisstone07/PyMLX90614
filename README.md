f5e798c00d24095a391d2158e063711ebd9a4942
Connor <cjkneebone@gmail.com>
2019-09-16 09:41:25 +1000

    Updated example

--- README.md
+++ README.md
@@ -27,9 +27,14 @@ $ sudo i2cdetect -y 1
 Within Python, the device can be used like this:
 
 ```python
-sensor = MLX90614(0x5A)
+from smbus2 import SMBus
+from mlx90614 import MLX90614
+
+bus = SMBus(1)
+sensor = MLX90614(bus, address=0x5A)
 print sensor.get_amb_temp()
 print sensor.get_obj_temp()
+bus.close()
 ```
 
 ## License
--- example.py
+++ example.py
@@ -1,9 +1,9 @@
-from mlx90614 import MLX90614
 from smbus2 import SMBus
+from mlx90614 import MLX90614
 
 if __name__ == "__main__":
-    address = 0x5A
     bus = SMBus(1)
-    sensor = MLX90614(bus, address)
+    sensor = MLX90614(bus, address=0x5A)
     print sensor.get_amb_temp()
     print sensor.get_obj_temp()
+    bus.close()
