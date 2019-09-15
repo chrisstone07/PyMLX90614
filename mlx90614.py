3f7eb0771b8dc0ac23421bb28408833605bbcba3
Connor <cjkneebone@gmail.com>
2019-09-16 08:00:35 +1000

    Documented constants

--- mlx90614.py
+++ mlx90614.py
@@ -4,22 +4,40 @@ from time import sleep
 
 class MLX90614():
 
+    # RAM offsets with 16-bit data, MSB first
+    # Raw data IR channel 1
     MLX90614_RAWIR1 = 0x04
+    # Raw data IR channel 2
     MLX90614_RAWIR2 = 0x05
+    # Ambient temperature
     MLX90614_TA = 0x06
+    # Object 1 temperature
     MLX90614_TOBJ1 = 0x07
+    # Object 2 temperature
     MLX90614_TOBJ2 = 0x08
 
+    # EEPROM offsets with 16-bit data, MSB first
+    # Object temperature max register
     MLX90614_TOMAX = 0x20
+    # Object temperature min register
     MLX90614_TOMIN = 0x21
+    # PWM configuration register
     MLX90614_PWMCTRL = 0x22
+    # Ambient temperature register
     MLX90614_TARANGE = 0x23
+    # Emissivity correction register
     MLX90614_EMISS = 0x24
+    # Configuration register
     MLX90614_CONFIG = 0x25
+    # Slave address register
     MLX90614_ADDR = 0x0E
+    # 1 ID register (read-only)
     MLX90614_ID1 = 0x3C
+    # 2 ID register (read-only)
     MLX90614_ID2 = 0x3D
+    # 3 ID register (read-only)
     MLX90614_ID3 = 0x3E
+    # 4 ID register (read-only)
     MLX90614_ID4 = 0x3F
 
     comm_retries = 5
