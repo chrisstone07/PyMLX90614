4e20fdc707cb8a5758ddd7371dcf98e6db899ce9
chrisstone07 <chrischris_4425@hotmail.com>
2020-09-08 22:56:08 +0530

    Update example.py

--- example.py
+++ example.py
@@ -4,6 +4,7 @@ from mlx90614 import MLX90614
 if __name__ == "__main__":
     bus = SMBus(1)
     sensor = MLX90614(bus, address=0x5A)
-    print sensor.get_amb_temp()
-    print sensor.get_obj_temp()
+    print(sensor.get_ambient())
+    print(sensor.get_object_1())
+    print(sensor.get_object_2())
     bus.close()
